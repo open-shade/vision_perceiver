c67c0795f33f915028564acafdcd70952b1645c3
Brandon Fan <brandonfan1256@gmail.com>
2022-07-08 13:58:18 -0400

    fixed issue with cloud build.sh

--- cloudbuild.sh
+++ cloudbuild.sh
@@ -1,7 +1,7 @@
 declare -a ROS_VERSIONS=( "foxy" "galactic" "humble" "rolling" )
 
 ORGANIZATION="deepmind"
-declare -a MODEL_VERSIONS=( "vision-perceiver-conv", "vision-perceiver-learned", "vision-perceiver-fourier" )
+declare -a MODEL_VERSIONS=( "vision-perceiver-conv" "vision-perceiver-learned" "vision-perceiver-fourier" )
 
 for VERSION in "${ROS_VERSIONS[@]}"
 do
@@ -10,7 +10,7 @@ do
     ROS_VERSION="$VERSION"
     gcloud builds submit --config cloudbuild.yaml . --substitutions=_ROS_VERSION="$ROS_VERSION",_MODEL_VERSION="$MODEL_VERSION",_ORGANIZATION="$ORGANIZATION" --timeout=10000 &
     pids+=($!)
-    echo Dispached "$MODEL_VERSION" on ROS "$ROS_VERSION"
+    echo Dispatched "$MODEL_VERSION" on ROS "$ROS_VERSION"
   done
 done
 
